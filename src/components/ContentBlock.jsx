0b54390bbb56bb5c7d1b3ff731ad4d6bd92d58e2
Brani <makkolot@gmail.com>
2020-06-09 10:14:16 +0300

    homework fixed v2

--- src/components/ContentBlock.jsx
+++ src/components/ContentBlock.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
-// Рендерит переданные от родительского компонента пропсыЖ заголовок и контент, 
+// Рендерит переданные от родительского компонента пропсы: заголовок и контент,
 // который в свою очередь может быть компонентом
 
 function ContentBlock(props) {
     return (
         <div>
-            <a href="#"><h4>{props.title}</h4></a>
-            {props.content}
+            <a href="#">
+                <h4>{props.title}</h4>
+            </a>
+            {props.children}
         </div>
-    )
+    );
 }
 
-export default ContentBlock
+export default ContentBlock;
--- src/components/ContentWidgetContainer.jsx
+++ src/components/ContentWidgetContainer.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
-import ContentBlock from './ContentBlock'
-import WeatherWidget from './WeatherWidget'
-import TVWidget from './TVWidget'
-import Visited from './Visited'
+import React from "react";
+import ContentBlock from "./ContentBlock";
+import WeatherWidget from "./WeatherWidget";
+import TVWidget from "./TVWidget";
+import Visited from "./Visited";
 
 // Рендерит список контентных блоков, в качестве пропсов передает заголовок и содержимое
 function ContentWidgetContainer() {
     return (
         <div className="ContentWidgetContainer__grid">
-            <ContentBlock title='Погода' content={<WeatherWidget />} />
+            <ContentBlock title="Погода">
+                <WeatherWidget />
+            </ContentBlock>
 
-            <ContentBlock title='Карта Германии' content='Расписания' />
+            <ContentBlock title="Карта Германии">
+                <a href="#">Расписания</a>
+            </ContentBlock>
 
-            <ContentBlock title='Эфир' content={<TVWidget />} />
+            <ContentBlock title="Эфир">
+                <TVWidget />
+            </ContentBlock>
 
-            <ContentBlock title='Посещаемое' content={<Visited />} />
+            <ContentBlock title="Посещаемое">
+                <Visited />
+            </ContentBlock>
 
-            <ContentBlock title='Телепрограмма' content={<TVWidget />} />
-
+            <ContentBlock title="Телепрограмма">
+                <TVWidget />
+            </ContentBlock>
         </div>
-    )
+    );
 }
 
-export default ContentWidgetContainer
+export default ContentWidgetContainer;
